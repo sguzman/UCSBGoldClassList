49c44ea66c570025d9e2e7d796dd1543b3141fe1
Salvador Guzman <guzmansalv@gmail.com>
2017-11-15 08:12:03 -0800

    added stub main and unit test

---
+++ src/test/scala/com/github/sguzman/scala/ucsb/gold/miner/MainTest.scala
@@ -0,0 +1,11 @@
+package com.github.sguzman.scala.ucsb.gold.miner
+
+class MainTest extends org.scalatest.FunSuite {
+  test("A test should succeed") {
+    assert(condition = true)
+  }
+
+  test("A test should succeed twice") {
+    assert(condition = true)
+  }
+}
