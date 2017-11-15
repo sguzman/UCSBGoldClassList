fee2e30a69a2d184688504a2c9377bb652dc37d9
Salvador Guzman <guzmansalv@gmail.com>
2017-11-15 09:04:15 -0800

    add jcommander args validator class

---
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/args/Args.scala
@@ -0,0 +1,56 @@
+package com.github.sguzman.scala.ucsb.gold.miner.args
+
+import com.beust.jcommander.Parameter
+
+class Args {
+  @Parameter(
+    names = Array("-u", "--user", "--username"),
+    description = "UCSB GOLD user name",
+    arity = 1,
+    echoInput = true,
+    hidden = false,
+    required = true,
+    password = false,
+    order = 1
+  )
+  var user: String = ""
+
+  @Parameter(
+    names = Array("-p", "--pass", "--password"),
+    description = "UCSB GOLD password",
+    arity = 1,
+    echoInput = false,
+    hidden = false,
+    required = true,
+    password = true,
+    help = false,
+    order = 2
+  )
+  var pass: String = ""
+
+  @Parameter(
+    names = Array("-o", "--old", "--oldfart"),
+    description = "UCSB GOLD password",
+    arity = 1,
+    echoInput = false,
+    hidden = false,
+    required = true,
+    password = true,
+    help = false,
+    order = 3
+  )
+  var old: Boolean = false
+
+  @Parameter(
+    names = Array("-h", "--help", "--helpme"),
+    description = "UCSB GOLD password",
+    arity = 1,
+    echoInput = false,
+    hidden = false,
+    required = true,
+    password = true,
+    help = true,
+    order = 4
+  )
+  var help: Boolean = false
+}
