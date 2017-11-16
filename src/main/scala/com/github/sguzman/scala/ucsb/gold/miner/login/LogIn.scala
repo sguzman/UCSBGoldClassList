0ab7ed6e7a1af776e84994a54934f2bd50da9d63
Salvador Guzman <guzmansalv@gmail.com>
2017-11-15 22:07:14 -0800

    added return value to main method

--- src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/login/LogIn.scala
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/login/LogIn.scala
@@ -1,18 +1,12 @@
 package com.github.sguzman.scala.ucsb.gold.miner.login
 
-import java.net.URI
-import java.nio.charset.StandardCharsets
-
 import com.beust.jcommander.JCommander
 import com.github.sguzman.scala.ucsb.gold.miner.args.Args
-import org.apache.http.client.methods.{HttpGet, HttpPost}
-import org.apache.http.entity.ByteArrayEntity
-import org.apache.http.impl.client.HttpClientBuilder
-import org.apache.http.util.EntityUtils
-import org.jsoup.Jsoup
+import com.machinepublishers.jbrowserdriver.{JBrowserDriver, Settings, Timezone}
+import org.openqa.selenium.{By, Cookie}
 
 object LogIn {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Set[Cookie]  = {
     val argv = new Args
     val j = JCommander.newBuilder()
       .addObject(argv)
@@ -24,76 +18,22 @@ object LogIn {
       System.exit(0)
     }
 
-    val client = HttpClientBuilder
-      .create
-      .build
-    val url = "https://my.sa.ucsb.edu/gold/login.aspx"
-
-    val uri = new URI(url)
-    val get = new HttpGet(uri)
-
-    get.addHeader("Host", "my.sa.ucsb.edu")
-    get.addHeader("Connection", "keep-alive")
-    get.addHeader("Upgrade-Insecure-Requests", "1")
-    get.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36")
-    get.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-    get.addHeader("DNT", "1")
-    get.addHeader("Referer", "https://www.google.com/")
-    get.addHeader("Accept-Encoding", "gzip, deflate, br")
-    get.addHeader("Accept-Language", "en-US,en;q=0.9")
-
-    val resp = client.execute(get)
-    val jsoup = Jsoup.parse(EntityUtils.toString(resp.getEntity))
-
-    val valueStr = (List(
-      "__LASTFOCUS",
-      "__VIEWSTATE",
-      "__VIEWSTATEGENERATOR",
-      "__EVENTTARGET",
-      "__EVENTARGUMENT",
-      "__EVENTVALIDATION",
-    ).map(s => if (jsoup.selectFirst("#" ++ s) != null) s ++ "=" ++ jsoup.selectFirst("#" ++ s).`val` else "").filter(_.nonEmpty) ++ (if (argv.old)
-      List(
-        "ctl00$pageContent$userNameText=",
-        "ctl00$pageContent$passwordText=",
-        "ctl00$pageContent$PermPinLogin$userNameText=" ++ argv.user,
-        "ctl00$pageContent$PermPinLogin$passwordText=" ++ argv.pass,
-        "ctl00$pageContent$PermPinLogin$loginButton.x=0",
-        "ctl00$pageContent$PermPinLogin$loginButton.y=0"
-      )
+    val (user, pass, submit) = if (argv.old)
+      ("pageContent_PermPinLogin_userNameText", "pageContent_PermPinLogin_passwordText", "pageContent_PermPinLogin_loginButton")
     else
-      List(
-        "ctl00$pageContent$userNameText=" ++ argv.user,
-        "ctl00$pageContent$passwordText=" ++ argv.pass,
-        "ctl00$pageContent$PermPinLogin$userNameText=",
-        "ctl00$pageContent$PermPinLogin$passwordText=",
-        "ctl00$pageContent$loginButton.x=0",
-        "ctl00$pageContent$loginButton.y=0"
-      ))).mkString("&")
+      ("pageContent_userNameText", "pageContent_passwordText", "pageContent_loginButton")
 
+    val jb = new JBrowserDriver(Settings.builder.timezone(Timezone.AMERICA_LOSANGELES).build)
+    val url = "https://my.sa.ucsb.edu/gold/login.aspx"
+    jb.get(url)
 
-    val a = new URI(url)
-    val b = new HttpPost(a)
+    val userText = jb.findElement(By.id(user))
+    val passText = jb.findElement(By.id(pass))
+    val button = jb.findElement(By.id(submit))
 
-    b.addHeader("Host", "my.sa.ucsb.edu")
-    b.addHeader("Connection", "keep-alive")
-    b.addHeader("Cache-Control", "max-age=0")
-    b.addHeader("Origin", "https://my.sa.ucsb.edu")
-    b.addHeader("Upgrade-Insecure-Requests", "1")
-    b.addHeader("Content-Type", "application/x-www-form-urlencoded")
-    b.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36")
-    b.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
-    b.addHeader("DNT", "1")
-    b.addHeader("Referer", "https://my.sa.ucsb.edu/gold/login.aspx")
-    b.addHeader("Accept-Encoding", "gzip, deflate, br")
-    b.addHeader("Accept-Language", "en-US,en;q=0.9")
-
-    val g = valueStr.getBytes(StandardCharsets.UTF_8)
-    val h = new ByteArrayEntity(g)
-    b.setEntity(h)
-    val c = client.execute(b)
-    val d = c.getEntity
-    val e = EntityUtils.toString(d)
-    println(e)
+    userText.sendKeys(argv.user)
+    passText.sendKeys(argv.pass)
+    button.click()
+    jb.manage.getCookies
   }
 }
