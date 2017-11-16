25eadb6405c2d59c2dec4b61a7733716fcd25801
Salvador Guzman <guzmansalv@gmail.com>
2017-11-16 12:00:23 -0800

    removed depedency on jsoup - jbrowserdriver all the way

--- src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/scrape/CourseScrape.scala
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/scrape/CourseScrape.scala
@@ -3,12 +3,12 @@ package com.github.sguzman.scala.ucsb.gold.miner.scrape
 import java.time.{ZoneId, ZonedDateTime}
 
 import com.machinepublishers.jbrowserdriver.JBrowserDriver
-import org.jsoup.Jsoup
+import org.openqa.selenium.By
 
 import scala.collection.JavaConverters._
 
 object CourseScrape {
-  def apply(jb: JBrowserDriver): (List[Int], List[String], List[(String, String)], JBrowserDriver) =
+  def apply(jb: JBrowserDriver): (List[Int], List[String], List[String], JBrowserDriver) =
     (years, quarters, departments(jb), jb)
 
   private def departments(jb: JBrowserDriver) = {
@@ -20,17 +20,13 @@ object CourseScrape {
 
   private def depts(jb: JBrowserDriver) = {
     val body = jb.getPageSource
-    val jsoup = Jsoup.parse(body)
     val id = "pageContent_subjectAreaDropDown"
+    val eles = jb.findElements(By.cssSelector(s"#$id > option"))
 
-    val selectDepts = jsoup.select(s"#$id > option").asScala
-    val deptOptions = for (d <- selectDepts) yield s"${d.text}"
+    val selectDepts = eles.asScala.map(_.getAttribute("value"))
+    val departments = selectDepts.tail.toList
 
-    val inner = deptOptions.tail.toList
-    val departments = inner.map(_.split(" - "))
-    val deptTuples = departments.map(d => (d.head, d.last))
-
-    deptTuples
+    departments
   }
 
   private def years =
