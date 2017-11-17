4bdcb0b8daabed19ebd9b1dfcecb85331f712694
Salvador Guzman <guzmansalv@gmail.com>
2017-11-17 03:13:42 -0800

    use jb to scrape class offerings and reset to previous page

--- src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/scrape/FineScrape.scala
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/scrape/FineScrape.scala
@@ -1,15 +1,19 @@
 package com.github.sguzman.scala.ucsb.gold.miner.scrape
 
+import com.github.sguzman.scala.ucsb.gold.miner.args.Args
+import com.github.sguzman.scala.ucsb.gold.miner.login.Login
 import com.machinepublishers.jbrowserdriver.JBrowserDriver
 import org.openqa.selenium.By
 import org.openqa.selenium.support.ui.Select
 
 object FineScrape {
   def apply(jb: JBrowserDriver, cls: List[String]): String = {
-    course(jb, cls.head)
+    cls.map(s => course(jb, s)).mkString
   }
 
   private def course(jb: JBrowserDriver, str: String) = {
+    println(s"Processing $str")
+
     val dropDownID = "pageContent_subjectAreaDropDown"
     val selectElement = jb.findElement(By.id(dropDownID))
     val select = new Select(selectElement)
@@ -18,6 +22,9 @@ object FineScrape {
     val searchID = "pageContent_searchButton"
     val buttonElement = jb.findElement(By.id(searchID))
     buttonElement.click()
+
+    val url = "https://my.sa.ucsb.edu/gold/BasicFindCourses.aspx"
+    jb.get(url)
 
     jb.getPageSource
   }
