75fa8f5a19df8a2f9561e8fe29c749b64551f623
Salvador Guzman <guzmansalv@gmail.com>
2017-11-17 18:59:58 -0800

    make var members val

--- src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/args/Args.scala
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/args/Args.scala
@@ -2,7 +2,7 @@ package com.github.sguzman.scala.ucsb.gold.miner.args
 
 import com.beust.jcommander.Parameter
 
-class Args {
+class Args(
   @Parameter(
     names = Array("-u", "--user", "--username"),
     description = "UCSB GOLD user name",
@@ -13,7 +13,7 @@ class Args {
     password = false,
     order = 1
   )
-  var user: String = ""
+  val user: String,
 
   @Parameter(
     names = Array("-p", "--pass", "--password"),
@@ -26,7 +26,7 @@ class Args {
     help = false,
     order = 2
   )
-  var pass: String = ""
+  val pass: String,
 
   @Parameter(
     names = Array("-o", "--old", "--oldfart"),
@@ -39,7 +39,7 @@ class Args {
     help = false,
     order = 3
   )
-  var old: Boolean = false
+  val old: Boolean,
 
   @Parameter(
     names = Array("-h", "--help", "--helpme"),
@@ -52,5 +52,5 @@ class Args {
     help = true,
     order = 4
   )
-  var help: Boolean = false
-}
+  val help: Boolean
+)
