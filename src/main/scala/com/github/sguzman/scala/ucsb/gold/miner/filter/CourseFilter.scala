848fc9a764aebf253fa936a114999c9ac8d16726
Salvador Guzman <guzmansalv@gmail.com>
2017-11-22 06:16:56 -0800

    course filter imple

--- src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/filter/CourseFilter.scala
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/filter/CourseFilter.scala
@@ -1,5 +1,18 @@
 package com.github.sguzman.scala.ucsb.gold.miner.filter
 
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+
+import scalaj.http.HttpResponse
+
 object CourseFilter {
-  def apply[A](a: A): A = a
+  def apply(resp: HttpResponse[String]) = {
+    val body = resp.body
+    val doc = JsoupBrowser().parseString(body)
+
+    val text1 = doc >> elementList("#pageContent_CourseList > tbody > tr > td > table")
+
+    text1.map(_.text)
+  }
 }
