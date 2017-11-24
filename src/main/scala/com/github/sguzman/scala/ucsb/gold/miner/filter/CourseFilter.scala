de4640076baaf080937d1d8ed087b2fbdb0dc6a6
Salvador Guzman <guzmansalv@gmail.com>
2017-11-24 11:35:31 -0800

    came up with a better method for extracting meaningful text 1. search all tags that contain an id (they should have good content) 2. use the way the resulting list of text is structure to figure out classes 3. profit

--- src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/filter/CourseFilter.scala
+++ src/main/scala/com/github/sguzman/scala/ucsb/gold/miner/filter/CourseFilter.scala
@@ -3,7 +3,6 @@ package com.github.sguzman.scala.ucsb.gold.miner.filter
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.model.Element
 
 import scalaj.http.HttpResponse
 
@@ -12,25 +11,9 @@ object CourseFilter {
     val body = resp.body
     val doc = JsoupBrowser().parseString(body)
 
-    val elements = (doc >> allText("#pageContent_criteriaLabel")) +:
-      (doc >> elementList("#pageContent_CourseList > tbody > tr"))
-        .zipWithIndex
-        .map(course)
-      .filter(_.head.nonEmpty)
+    val head = doc >> elementList("[id]")
+    val text = head.map(_.text)
 
-    elements
-  }
-
-  def noEmpty[A](courseList: List[A]): Boolean = {
-    courseList.nonEmpty
-  }
-
-  def course(tup: (Element, Int)) = {
-    val doc = tup._1
-    val idx = tup._2
-    List(
-      doc >> allText(s"#pageContent_CourseList_PermNbr_$idx"),
-      doc >> allText(s"#pageContent_CourseList_PrimaryRow_$idx")
-    )
+    text
   }
 }
